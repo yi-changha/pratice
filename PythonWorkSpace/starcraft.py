fe40d512abcd7686bab582b6e66969cbbd611252
yi-changha <77517634+yi-changha@users.noreply.github.com>
2021-12-15 13:53:48 +0900

    Add files via upload

---
+++ PythonWorkSpace/starcraft.py
@@ -0,0 +1,16 @@
+# # input : 사용자 입력을 받는 함수
+# language = input("무슨 언어를 좋아하세요?")
+# print("{0}은 아주 좋은 언어입니다!".format(language))
+
+# print(dir())
+# import random
+
+# # print(dir())
+# # import pickle
+
+# # print(dir(random))
+lst = [1, 2, 3]
+print(dir(lst))
+
+# name = "Jim"
+# print(dir(name))
