083e0414dfd33def6bf1273828ce6dc09d460c87
yi-changha <77517634+yi-changha@users.noreply.github.com>
2022-01-12 14:56:12 +0900

    Add files via upload

---
+++ CWorkSpace/Basic/05-1-자료형.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main() {
+	/*
+	// int : 32비트(4바이트), 정수를 담는 데 쓰임
+	int a = 5;
+	int b = 3;
+
+	int hap = a + b;
+	int cha = a - b;
+	int gop = a * b;
+	int mok = a / b;
+	int namuji = a % b;
+
+	printf("%d + %d = %d\n", a, b, hap);
+	printf("%d - %d = %d\n", a, b, cha);
+	printf("%d * %d = %d\n", a, b, gop);
+	printf("%d / %d = %d\n", a, b, mok);
+	printf("%d %% %d = %d\n", a, b, namuji);
+*/
+
+	/*
+	// float : 32비트(4바이트), 실수를 담는 데 쓰임
+	float a = 5;
+	float b = 3;
+
+	float hap = a + b;
+	float cha = a - b;
+	float gop = a * b;
+	float mok = a / b;
+
+	printf("%f + %f = %f\n", a, b, hap);
+	printf("%f - %f = %f\n", a, b, cha);
+	printf("%f * %f = %f\n", a, b, gop);
+	printf("%f / %f = %f\n", a, b, mok);
+	*/
+
+	// double : 64비트(8바이트), 실수를 담는 데 쓰임
+	double a = 5;
+	double b = 3;
+
+	double hap = a + b;
+	double cha = a - b;
+	double gop = a * b;
+	double mok = a / b;
+
+	printf("%f + %f = %f\n", a, b, hap);
+	printf("%f - %f = %f\n", a, b, cha);
+	printf("%f * %f = %f\n", a, b, gop);
+	printf("%f / %f = %f\n", a, b, mok);
+
+
+	// 부동소수점(floating point)
+}
