aabbdc2433c327d5ba1940a72f65eac1968ad9c6
yi-changha <77517634+yi-changha@users.noreply.github.com>
2022-01-12 14:24:58 +0900

    Add files via upload

---
+++ CWorkSpace/Basic/04-1-변수.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	int a; // 선언 : 변수를 만드는 과정
+	// int : 변수, 32비트 = 4Bytes
+
+	a = 3; // 대입 : a에 3을 넣겠다(O), a와 3이 같다(X)
+	printf("%d\n", a);
+
+	printf("%c\n", 'a');
+
+	a = 5; // 대입
+	printf("%d\n", a);
+
+}
---
+++ CWorkSpace/Basic/04-2-변수.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	int a = 2; // 초기화
+	int b = 3;
+
+	printf("%d + %d = %d\n", a, b, a + b);
+
+	// 변수 이름 : 알파벳 대소문자, _, 숫자(첫번째 글자)
+
+	int hap = a + b;
+
+	printf("%d + %d = %d\n", a, b, hap);
+}
