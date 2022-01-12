5b9e20ddbe3b73b9bd54ff4b728cf3378061d071
yi-changha <77517634+yi-changha@users.noreply.github.com>
2022-01-12 12:21:19 +0900

    Add files via upload

---
+++ CWorkSpace/ex01-HelloWorld.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+//#include : stdio.h를 포함
+//stdio.h : 언어를 설정해 놓은 파일
+
+int main() {
+//main() : 함수, 중괄호 안의 내용을 실행
+
+	printf("Hello, World!\n");
+	printf("Hello, World!\n");
+	printf("Hello, World!\n");
+	printf("Hello, World!\n");
+	printf("Hello, World!\n");
+	printf("Hello, World!");
+	printf("Hello, World!");
+	printf("Hello, World!\n");
+	printf("Hello, World!\nHello, World!\nHello, World!\n\n\n");
+	//printf() : 함수, 괄호 안의 내용을 출력.
+	// ; : 마침표, 문장의 끝을 알림.
+
+	//example
+	printf("example)\n동해물과 백두산이\n마르고\n닳도록\n\n");
+	printf("동해물과 백두산이\n");
+	printf("마르고\n");
+	printf("닳도록\n");
+}
