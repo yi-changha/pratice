4a1167af67888a9e91b97446e25a06112cb6de72
yi-changha <77517634+yi-changha@users.noreply.github.com>
2022-01-13 17:28:58 +0900

    Add files via upload

---
+++ CWorkSpace/Basic/22-2-PaslcalsTriangle.cpp
@@ -0,0 +1,28 @@
+// ÆÄ½ºÄ®ÀÇ »ï°¢Çü
+
+/*
+	1
+	1 1
+	1 2 1
+	1 3 3 1
+*/
+
+#include <stdio.h>
+
+int main() {
+	int p[10][10];
+
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j <= i; j++) {
+			if (j == 0 || j == i) {
+				p[i][j] = 1;
+			}
+			else {
+				p[i][j] = p[i - 1][j - 1] + p[i - 1][j];
+			}
+
+			printf("%d ", p[i][j]);
+		}
+		printf("\n");
+	}
+}
