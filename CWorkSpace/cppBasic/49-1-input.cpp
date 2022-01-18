11547264977445fb64a99ef407a3529008d39326
yi-changha <77517634+yi-changha@users.noreply.github.com>
2022-01-18 13:19:57 +0900

    Add files via upload

---
+++ CWorkSpace/cppBasic/49-1-input.cpp
@@ -0,0 +1,13 @@
+#include <iostream> // input, output stream
+
+using namespace std;
+
+int main() {
+
+	// std : 성(이름) name space(이름 공간) 
+	// cout : << 로 데이터 분리
+	// end : end line, 줄바꿈
+//	std::cout << "Hello, World!" << std::endl;
+
+	cout << "Hello, World!" << endl;
+}
