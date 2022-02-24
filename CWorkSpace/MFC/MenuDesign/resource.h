b8f281576f13c004e6f398953d55f35bcb4a4334
yi-changha <77517634+yi-changha@users.noreply.github.com>
2022-02-24 16:00:31 +0900

    Add files via upload

---
+++ CWorkSpace/MFC/MenuDesign/resource.h
@@ -0,0 +1,43 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// MenuDesign.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_MenuDesignTYPE              130
+#define ID_32771                        32771
+#define ID_32772                        32772
+#define ID_32773                        32773
+#define ID_TEST_ITEM1                   32774
+#define ID_TEST_ITEM3                   32775
+#define ID_32776                        32776
+#define ID_32777                        32777
+#define ID_32778                        32778
+#define ID_32779                        32779
+#define ID_ITEM2_4                      32780
+#define ID_ITEM2_3                      32781
+#define ID_ITEM2_2                      32782
+#define ID_ITEM2_1                      32783
+#define ID_32784                        32784
+#define ID_32785                        32785
+#define ID_32786                        32786
+#define ID_32787                        32787
+#define ID_32788                        32788
+#define ID_32789                        32789
+#define ID_MATHFUNC_ABS                 32790
+#define ID_MATHFUNC_DIV                 32791
+#define ID_MATHFUNC_LOG                 32792
+#define ID_MATHFUNC_SIN                 32793
+#define ID_MATHFUNC_COS                 32794
+#define ID_MATHFUNC_TAN                 32795
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        310
+#define _APS_NEXT_COMMAND_VALUE         32796
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
---
+++ CWorkSpace/MFC/mfc_test10_key1/ChildView.cpp
@@ -0,0 +1,108 @@
+﻿
+// ChildView.cpp: CChildView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "mfc_test10_key1.h"
+#include "ChildView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChildView
+
+CChildView::CChildView() // 생성자 메모리 할당.
+{
+	m_xPos = m_yPos = 100; // 임의의 값으로 초기화
+	m_bFill = FALSE;  //도형 내부를 채우지 않음
+}
+
+CChildView::~CChildView()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+	ON_WM_SIZE()
+	ON_WM_KEYDOWN()
+END_MESSAGE_MAP()
+
+
+
+// CChildView 메시지 처리기
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs) 
+{
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW|CS_VREDRAW|CS_DBLCLKS, 
+		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW+1), nullptr);
+
+	return TRUE;
+}
+
+void CChildView::OnPaint() 
+{
+	CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+	if (m_bFill == TRUE) dc.SelectStockObject(BLACK_BRUSH);  // m_bFill 이 TRUE 이면 내부를 검은색으로 채운다.
+	/*
+	CBrush brush1(RGB(255, 0, 0));
+	dc.SelectObject(&brush1);
+	*/
+	dc.Ellipse(m_xPos - 20, m_yPos - 20, m_xPos + 20, m_yPos + 20);  // 타원 x1, y1, x2, y2
+}
+
+
+
+void CChildView::OnSize(UINT nType, int cx, int cy)
+{
+	CWnd::OnSize(nType, cx, cy);
+	m_xClient = cx; // 클라이언트 영역 x좌표 픽셀값
+	m_yClient = cy; // 클라이언트 영역 y좌표 픽셀값
+}
+
+
+void CChildView::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
+// 키보드 메시지 핸들러
+// UINT nChar => 키에 할당된 가상 키 코드값을 가진다.(운영체제가 각각 키에 할당한 고유값)
+// UINT nRepCnt => 키를 게속 누르고 있으면 1보다 큰 값을 가질 수 있다.
+// UINT nFlags => 키와 관련된 부가적 정보(스캔 코드, 확장 키 여부...)
+
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	switch (nChar) {
+	case VK_LEFT:  // 왼쪽 이동 가상 키코드 
+		m_xPos -= 20;  // 20픽셀 이동
+		break;
+	case VK_RIGHT:  // 오른쪽 이동 가상 키코드
+		m_xPos += 20;
+		break;
+	case VK_UP:  // 위로 이동 가상 키코드
+		m_yPos -= 20;
+		break;
+	case VK_DOWN:  // 아래로 이동 가상 키코드
+		m_yPos += 20;
+		break;
+	case VK_HOME:  // HOME 이동 가상 키코드
+		m_xPos += -10000;  // 가장 작은값으로 설정하여 좌상단으로 이동
+		m_yPos += -10000;
+		break;
+	case VK_SPACE:  // 스페이스바를 눌렀을때 가상 키코드
+		m_bFill = !m_bFill;  // 도형이 반전(검정색)
+	}
+
+	//20 <= m_xPos <= m_xClient - 20;
+	m_xPos = min(max(20, m_xPos), m_xClient - 20);  // 도형의 위치가 클라이언트 영역을 벋어나지 않게 하는 min(), max() 매크로함수
+	// m_xPos = max(20, m_xPos), m_xClient - 20;  // 도형의 위치가 클라이언트 영역을 벋어남
+	//20 <= m_yPos <= m_yClient - 20;
+	m_yPos = min(max(20, m_yPos), m_yClient - 20);
+
+	Invalidate();  // 클라이언트 영역 전체를 무효화 함으로써 WM_PAINT 메시지를 강제로 발생시킨다. 즉 화면을 갱신한다
+}
