0efb0e78464062f82fd3b2c530e831fb271ef31a
yi-changha <77517634+yi-changha@users.noreply.github.com>
2022-03-06 16:59:45 +0900

    Add files via upload

---
+++ CWorkSpace/MFC/Subclass/CMyEdit.cpp
@@ -0,0 +1,41 @@
+﻿// CMyEdit.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "Subclass.h"
+#include "CMyEdit.h"
+
+
+// CMyEdit
+
+IMPLEMENT_DYNAMIC(CMyEdit, CEdit)
+
+CMyEdit::CMyEdit()
+{
+
+}
+
+CMyEdit::~CMyEdit()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CMyEdit, CEdit)
+	ON_WM_CHAR()
+END_MESSAGE_MAP()
+
+
+
+// CMyEdit 메시지 처리기
+
+
+
+
+void CMyEdit::OnChar(UINT nChar, UINT nRepCnt, UINT nFlags)
+{
+	if ((nChar >= _T('0') && nChar <= _T('9'))
+		|| nChar == _T('-') || nChar == _T('\b'))
+	{
+		CEdit::OnChar(nChar, nRepCnt, nFlags);
+	}
+}
