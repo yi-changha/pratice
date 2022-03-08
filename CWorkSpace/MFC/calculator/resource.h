ed19af63b9beb00909a9d770fce4424d2833996d
yi-changha <77517634+yi-changha@users.noreply.github.com>
2022-03-08 11:37:52 +0900

    Add files via upload

---
+++ CWorkSpace/MFC/calculator/resource.h
@@ -0,0 +1,39 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// calculator.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_CALCULATOR_DIALOG           102
+#define IDR_MAINFRAME                   128
+#define IDC_B7                          1000
+#define IDC_B8                          1001
+#define IDC_B9                          1002
+#define IDC_B4                          1004
+#define IDC_B5                          1005
+#define IDC_B6                          1006
+#define IDC_MINUS                       1007
+#define IDC_B1                          1008
+#define IDC_B2                          1009
+#define IDC_B3                          1010
+#define IDC_MUL                         1011
+#define IDC_B0                          1012
+#define IDC_DOT                         1013
+#define IDC_EQUAL                       1014
+#define IDC_DIV                         1015
+#define IDC_LOTTO                       1019
+#define IDC_EDIT1                       1020
+#define IDC_PLUS                        1021
+#define IDC_ERASE                       1022
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1021
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
