7c04753c5338af88483f16bfed12b218a1f905ce
yi-changha <77517634+yi-changha@users.noreply.github.com>
2022-03-24 16:47:23 +0900

    Update cone.cpp

--- CWorkSpace/OpenCascade/basic_3d_shapes/cone.cpp
+++ CWorkSpace/OpenCascade/basic_3d_shapes/cone.cpp
@@ -3,8 +3,11 @@ Open CASCADE 튜토리얼 by 이창하
 2022.03.24
 Cone 원뿔
 */
+#define _USE_MATH_DEFINES_
+
 #include "Viewer.h"
 
+#include <cmath>
 // OCC
 #include <gp_Ax2.hxx>
 #include <TopoDS_Shape.hxx>
@@ -17,13 +20,16 @@ int main(int argc, char** argv)
 	Viewer vout(50, 50, 500, 500);
 
 	gp_Ax2 yAxis;
-	yAxis.SetLocation(gp_Pnt(10.0, 10.0, 0.0));
 
-	TopoDS_Shape cone = BRepPrimAPI_MakeCone(yAxis, 5.0, 1.0, 10.0).Shape();
 	// Cone(gp_Ax2 중심축, 아래 반지름, 위 반지름, 높이)
+	yAxis.SetLocation(gp_Pnt(10.0, 10.0, 0.0));
+	TopoDS_Shape cone1 = BRepPrimAPI_MakeCone(yAxis, 5.0, 1.0, 10.0).Shape();
+	
+	// 잘라진 Cone(gp_Ax2 중심축, 아래 반지름, 위 반지름, 높이, 구현할 각도)
+	yAxis.SetLocation(gp_Pnt(-10.0, -10.0, 0.0));
+	TopoDS_Shape cone2 = BRepPrimAPI_MakeCone(yAxis, 5.0, 1.0, 10.0, M_PI/2).Shape();
 
-
-	vout << cone;
+	vout << cone1 << cone2;
 
 	vout.StartMessageLoop();
 	return 0;
