a59aee2411c2a3c94998aa2bcc03e9b6c6c3eb52
yi-changha <77517634+yi-changha@users.noreply.github.com>
2022-03-30 15:54:44 +0900

    Add files via upload

---
+++ project/calculator/calculatorDlg.h
@@ -0,0 +1,57 @@
+﻿
+// calculatorDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CcalculatorDlg 대화 상자
+class CcalculatorDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CcalculatorDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CALCULATOR_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CString m_result; // 에디트 컨트롤 변수
+	CString m_buf; // 숫자(문자)
+	bool m_bEqualClk;
+	int m_cType;
+	afx_msg void OnBnClickedErase();
+	afx_msg void OnBnClickedB0();
+	afx_msg void OnBnClickedB1();
+	afx_msg void OnBnClickedB2();
+	afx_msg void OnBnClickedB3();
+	afx_msg void OnBnClickedB4();
+	afx_msg void OnBnClickedB5();
+	afx_msg void OnBnClickedB6();
+	afx_msg void OnBnClickedB7();
+	afx_msg void OnBnClickedB8();
+	afx_msg void OnBnClickedB9();
+	afx_msg void OnBnClickedDot();
+	afx_msg void OnBnClickedEqual();
+	afx_msg void OnBnClickedLotto();
+	afx_msg void OnBnClickedPlus();
+	afx_msg void OnBnClickedMinus();
+	afx_msg void OnBnClickedMul();
+	afx_msg void OnBnClickedDiv();
+};
