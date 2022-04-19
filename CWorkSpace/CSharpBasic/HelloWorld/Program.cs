b9c4bac677cd1b4db22a1741ad6110441e6cd654
yi-changha <77517634+yi-changha@users.noreply.github.com>
2022-04-19 13:27:35 +0900

    Add files via upload

---
+++ CWorkSpace/CSharpBasic/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿// Hello World
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("안녕 C#");
+            Console.WriteLine("안녕하세요. "+ args[0]);
+
+            Console.ReadKey();          // 키 하나를 받을 때까지 기다림.
+        }
+    }
+}
